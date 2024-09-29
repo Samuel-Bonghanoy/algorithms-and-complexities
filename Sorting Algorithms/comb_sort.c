fb748774fc84ca634c33e83a95103ce289816be4
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-09-29 22:53:01 +0800

    feat: add comb sort

--- Sorting Algorithms/comb_sort.c
+++ Sorting Algorithms/comb_sort.c
@@ -14,18 +14,59 @@ int newgap(int gap)
         gap = 1;
     return gap;
 }
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
  
 /*Function to implement the combsort*/
-void combsort(int a[], int size)
+void combsort(int arr[], int size)
 {
-    
+    int x;
+
+    int pairCount, hasSwapped = 1;
+
+      for (int gap = size / 1.3; gap >= 1 ; gap /= 1.3){
+        hasSwapped = 0;
+
+        for (int x = 0; x < size - gap; x++)
+        {
+          if (arr[x] > arr[x + gap])
+          {
+            swap(&arr[x], &arr[x + gap]);
+            hasSwapped = 1;
+          }
+        }
+    }
+
 }
+
+void combsort2(int arr[], int size)
+{
+   int x, gap;
+
+   for(gap = size / 1.3; gap >= 1; gap /= 1.3){
+
+    for(x = 0; x < size - gap; x++){
+      if(arr[x] > arr[x + gap]){
+        swap(&arr[x], &arr[x+gap]);
+      }
+    }
+   }
+
+}
+
+
 int main ()
 {
     int n, i;
     int arr[] = { 12, 11, 13, 5, 6, 1 };
     int size = sizeof(arr) / sizeof(arr[0]);
-    combsort(arr, size);
+    combsort2(arr, size);
     printf("Sorted list:\n");        // Display the sorted array
     for(i = 0;i < size;i++)
     {
